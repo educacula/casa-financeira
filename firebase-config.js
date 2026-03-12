f572c7d13c7e68cf4c251da597b87128bb3abac9
educacula <educacula@gmail.com>
2026-03-12 11:06:16 -0300

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,19 +1,8 @@
-<script type="module">
-  // Import the functions you need from the SDKs you need
-  import { initializeApp } from "https://www.gstatic.com/firebasejs/12.10.0/firebase-app.js";
-  // TODO: Add SDKs for Firebase products that you want to use
-  // https://firebase.google.com/docs/web/setup#available-libraries
-
-  // Your web app's Firebase configuration
-  const firebaseConfig = {
-    apiKey: "AIzaSyB17j6xYMVnX20OW2Cgb5ibO5I52g4zA4E",
-    authDomain: "casa-financeira-ae08e.firebaseapp.com",
-    projectId: "casa-financeira-ae08e",
-    storageBucket: "casa-financeira-ae08e.firebasestorage.app",
-    messagingSenderId: "586081436813",
-    appId: "1:586081436813:web:df04229f1287dd2806cec4"
-  };
-
-  // Initialize Firebase
-  const app = initializeApp(firebaseConfig);
-</script>
+export const firebaseConfig = {
+  apiKey: "AIzaSyB17j6xYMVnX2OOW2Cgb5ib05I52g",
+  authDomain: "casa-financeira-ae08e.firebaseapp.com",
+  projectId: "casa-financeira-ae08e",
+  storageBucket: "casa-financeira-ae08e.firebasestorage.app",
+  messagingSenderId: "586081436813",
+  appId: "1:586081436813:web:df04229f1287dd280"
+};
